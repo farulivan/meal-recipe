92a9de3e29dc08af467371e4f5c1b7b5cec9237d
Farul Ivan <farulivan@gmail.com>
2022-09-19 10:41:35 +0700

    add Recipe Detail Feature

--- src/components/recipe-list.js
+++ src/components/recipe-list.js
@@ -1,6 +1,9 @@
+import axios from "axios";
+
 class RecipeList extends HTMLElement {
     constructor(){
         super()
+        this.showRecipeDetail = false
     }
     
     set meal(meal) {
@@ -8,10 +11,32 @@ class RecipeList extends HTMLElement {
         this.render()
     }
 
+    async recipeDetail(e){
+        const getDetail = async () => {
+        return await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${e}`)
+                .then(response => response.data.meals[0])
+                .catch(error => console.log(error.message))
+        }
+        getDetail().then(recipe => {
+            this.innerHTML = `
+            <div class="col-span-2" id="${recipe.idMeal}">
+                <img class="rounded-xl" src="${recipe.strMealThumb}" />
+                <h3 class="text-2xl font-extrabold text-emerald-700 mt-2">${recipe.strMeal}</h3>
+                <p class="text-md font-bold text-slate-500">${recipe.strArea} Cuisine</p>
+            </div>
+            `
+        })
+    }
+
+    connectedCallback(){
+        const recipeList = document.querySelector('recipe-list')
+        recipeList.addEventListener('click', (e) => this.recipeDetail(e.path[1].id))
+    }
+
     render(){
         this.innerHTML = this._meal.map(recipe => {
             return `
-                <div>
+                <div id="${recipe.idMeal}">
                     <img class="rounded-xl" src="${recipe.strMealThumb}" />
                     <p class="text-md font-bold text-slate-500">${recipe.strMeal}</p>
                 </div>
